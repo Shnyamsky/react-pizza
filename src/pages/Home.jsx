e869ab30b2e1905c52a34766334db0f7bebd96f2
Shnyamsky <ivanov.ilya-iv@yandex.ru>
2022-07-31 02:09:58 +0500

    Added search, pagination and context

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,18 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { SearchContext } from '../App';
 
 import Categories from '../components/Categories';
 import PizzaBlock from '../components/PizzaBlock';
 import Skeleton from '../components/PizzaBlock/Skeleton';
 import Sort from '../components/Sort';
+import Pagination from '../components/Pagination';
 
 const Home = () => {
+  const { searchValue } = useContext(SearchContext);
+
   const [items, setItems] = useState([]);
   const [isLoading, setIsloading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
 
   const [categoryId, setCategoryId] = useState(0);
   const [sortType, setSortType] = useState({
@@ -20,11 +25,12 @@ const Home = () => {
       try {
         setIsloading(true);
         const category = categoryId > 0 ? `category=${categoryId}` : '';
+        const searchItems = searchValue ? `search=${searchValue}` : '';
         const sortBy = sortType.sortProperty.replace('-', '');
         const order = sortType.sortProperty.includes('-') ? 'desc' : 'asc';
 
         await fetch(
-          `https://62e016ec98dd9c9df60d8371.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}`,
+          `https://62e016ec98dd9c9df60d8371.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}&${searchItems}`,
         )
           .then((res) => res.json())
           .then((json) => {
@@ -38,7 +44,16 @@ const Home = () => {
     }
     fetchData();
     window.scrollTo(0, 0);
-  }, [categoryId, sortType]);
+  }, [currentPage, searchValue, categoryId, sortType]);
+
+  const renderItems = () => {
+    // const filteredItems = items.filter((item) =>
+    //   item.title.toLowerCase().includes(searchValue.toLowerCase()),
+    // );
+    return isLoading
+      ? [...new Array(4)].map((_, index) => <Skeleton key={index} />)
+      : items.map((item) => <PizzaBlock key={item.id} {...item} />);
+  };
 
   return (
     <div className="container">
@@ -47,11 +62,8 @@ const Home = () => {
         <Sort sortType={sortType} onChangeSort={(obj) => setSortType(obj)} />
       </div>
       <h2 className="content__title">Все пиццы</h2>
-      <div className="content__items">
-        {isLoading
-          ? [...new Array(8)].map((_, index) => <Skeleton key={index} />)
-          : items.map((item) => <PizzaBlock key={item.id} {...item} />)}
-      </div>
+      <div className="content__items">{renderItems()}</div>
+      <Pagination onChangePage={(number) => setCurrentPage(number)} />
     </div>
   );
 };
