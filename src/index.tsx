e4f8639cc8b7e6c153c384a8d9c71906fd43429d
Shnyamsky <ivanov.ilya-iv@yandex.ru>
2022-11-06 06:41:24 +0600

    deplpoy to github pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
@@ -14,10 +14,10 @@ if (rootElem) {
   const root = ReactDOM.createRoot(rootElem);
 
   root.render(
-    <BrowserRouter>
+    <HashRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>,
+    </HashRouter>,
   );
 }
